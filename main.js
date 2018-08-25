7a1ae0115b83d1d47d108acaffae7d3fbf876ee5
Lauren Battle <LunarGem@users.noreply.github.com>
2018-08-24 22:29:03 -0400

    stat vars and addTurn
    change certain stats after certain actions

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ var breath = 0;
 var beats = 0;
 var thoughts = 0;
 var blinks = 0;
+var age = 0;
+var happiness = 100;
+var hunger = 0;
+var knowledge = 0;
+
 
 function breathe(){
     breath++;
@@ -23,6 +28,7 @@ function beat(){
 
 function think(){
     thoughts++;
+    addTurn();
     //console.log(thoughts);
     $('.thoughts').html(thoughts);
     if(thoughts == 1){
@@ -35,6 +41,12 @@ function blink(){
     blinks++;
     //console.log(blinks);
     $('.blinks').html(blinks);
+}
+
+function addTurn(){
+    age += 0.05; //to be balanced
+    happiness -= 0.05;
+    hunger += 0.05;
 }
 
 function addBeat(){
