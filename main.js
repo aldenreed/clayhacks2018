ffb258c8b8e8636cfbbc74c112390f4b017bcf5a
Lauren Battle <LunarGem@users.noreply.github.com>
2018-08-24 22:08:29 -0400

    add heart and brain
    beats and thoughts follow the same pattern

--- main.js
+++ main.js
@@ -4,6 +4,35 @@ var thoughts = 0;
 
 function breathe(){
     breath++;
-    console.log(breath);
+    //console.log(breath);
     $('.breaths').html(breath);
+    if(breath == 10){
+        addBeat();
+    }
+}
+
+function beat(){
+    beats++;
+    //console.log(beats);
+    $('.breaths').html(beats);
+    if(beats == 10){
+        addThought();
+    }
+}
+
+function think(){
+    thoughts++;
+    //console.log(thoughts);
+    $('.thoughts').html(thoughts);
+    if(thoughts == 1){
+        transformColor();
+    }
+}
+
+function addBeat(){
+    $('.heart').show();
+}
+
+function addThought(){
+    $('.brain').show();
 }
